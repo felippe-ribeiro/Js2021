e817d7c0528ba155e8cce91844ac76dd22a85797
felippe-ribeiro <felippe.bribeiro@gmail.com>
2021-12-03 13:16:52 -0300

    Include Node Course

---
+++ funcao/async_await_2.js
@@ -0,0 +1,35 @@
+function gerarNumerosEntre(min, max,numerosProibidos){
+    if(min > max) [max, min] = [min, max]
+
+    return new Promise(resolve => {
+        const fator = max - min + 1
+        const aleatorio = parseInt(Math.random() * fator) + min
+        if(numerosProibidos.includes(aleatorio)){
+            ('Numero repitido')
+        }else{
+            resolve(aleatorio)
+        }
+    })
+}
+
+async function gerarMegaSena(qtdeNumeros,tentativas = 1){
+    try {
+        const numeros = []
+        for(let _ of Array(qtdeNumeros).fill()){
+            numeros.push(await gerarNumerosEntre(1,60, numeros))
+        }
+        return numeros
+    } catch(e) {
+        if(tentativas > 10)
+        {
+            throw "Muitas tentativas"
+        }
+        else{
+            gerarMegaSena(qtdeNumeros, tentativas + 1)
+        }
+    }
+}
+
+gerarMegaSena(6)
+    .then(console.log)
+    .catch(console.log)
